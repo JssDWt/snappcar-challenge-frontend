7d66ec1d17581d3551599a34b2d308dfc2db664a
Jesse de Wit <jessedewit@MBP-van-Jesse.lan>
2019-09-28 14:07:18 +0200

    removed account from routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { AuthGuard } from './_guards';
+// import { AuthGuard } from './_guards';
 import { CarsComponent } from './cars';
 import { CarDetailsComponent } from './car-details';
-import { AccountComponent } from './account';
+// import { AccountComponent } from './account';
 import { LoginComponent } from './login';
 
 const routes: Routes = [
   { path: 'cars', component: CarsComponent },
   { path: 'cars/:id', component: CarDetailsComponent },
-  { path: 'account', component: AccountComponent, canActivate: [AuthGuard] },
+  // { path: 'account', component: AccountComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
   { path: '', redirectTo: 'cars', pathMatch: 'full' },
   { path: '**', redirectTo: '' },
